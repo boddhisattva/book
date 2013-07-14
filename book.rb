cb8b64db3b2d0ab167b48f1d7c251094433dc49a
boddhisattva <mail4mohnish@yahoo.in>
2013-07-14 19:47:36 +0530

    code clean up

--- book.rb
+++ book.rb
@@ -8,8 +8,7 @@ class Book
     count = 0
     @books = @books.inject({}) { |hash, book|
       print "#{book.first}: "
-      price = gets.chomp
-      #while ((duration.to_i <= 0  || duration.include?(".") || /^[-+]?[0-9]+$/.match(duration).nil?) && duration != "lightning")
+      price = gets.chomp      
       while (price !~ /^[1-9]\d*$/ && price != "second hand")
         puts "Price can't be 0 or a negative integer or in decimal format or alphanumeric. \nPlease input appropriate duration in integer"
         price = gets.chomp
@@ -20,23 +19,7 @@ class Book
     }
   end
 
-  #def get_duration
-  #  puts "Welcome to Conference Talks Scheduler Program, to start with, may I request you to enter the duration for each talk in minutes:-"
-  #  @talks = @talks.inject({}) { |hash, talk|
-  #    print "#{talk.first}: "
-  #    duration = gets.chomp
-  #    #while ((duration.to_i <= 0  || duration.include?(".") || /^[-+]?[0-9]+$/.match(duration).nil?) && duration != "lightning")
-  #    while (duration !~ /^[1-9]\d*$/ && duration != "lightning")
-  #      puts "Duration of a talk can't be 0 or a negative integer or in decimal format or alphanumeric. \nPlease input appropriate duration in integer minutes for #{talk.first} talk:"
-  #      duration = gets.chomp
-  #    end
-  #    duration == "lightning" ? duration = "5" : duration
-  #    hash[talk.first] = duration.to_i
-  #    hash
-  #  }
-  #end
-
-end
+ end
 
 books = {"The Last Samurai" => nil,
          "Ruby Cookbook" =>  nil,
@@ -48,9 +31,3 @@ books = {"The Last Samurai" => nil,
 book_details = Book.new(books)
 book_details.get_prices
 
-#books = 10
-#puts "enter title"
-#title = gets.chomp
-#puts "enter
-#
-#Book.new
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,27 +3,17 @@ require 'spec_helper'
 describe Book do
 
   before :each do
-    talks = {"Writing Fast Tests Against Enterprise Rails" => nil,
-             "Overdoing it in Python" =>  nil,
-             "Lua for the Masses" =>  nil,
-             "Ruby Errors from Mismatched Gem Versions" =>  nil,
-             "Common Ruby Errors" =>  nil,
-             "Rails for Python Developers" =>  nil,
-             "Communicating Over Distance" =>  nil,
-             "Accounting-Driven Development" =>  nil,
-             "Woah" =>  nil,
-             "Sit Down and Write" =>  nil,
-             "Pair Programming vs Noise" =>  nil,
-             "Rails Magic" =>  nil,
-             "Ruby on Rails: Why We Should Move On" =>  nil,
-             "Clojure Ate Scala (on my project)" =>  nil}
-    sessions = Hash[:morning => 180, :afternoon =>240]
-    @conf_scheduler_test = Book.new(talks,sessions,:entry_new)
+    books = {"The Last Samurai" => nil,
+         "Ruby Cookbook" =>  nil,
+         "Rails Recipes" =>  nil,
+         "Agile Development with Rails" =>  nil,
+         "Harry Potter and the Deathly Hallows" =>  nil}
+    @book = Book.new(books)
   end
 
   describe "#new" do
-    it "Should be an instance of the TalksScheduler" do
-      @conf_scheduler_test.should be_an_instance_of Book
+    it "Should be an instance of the Book" do
+      @book.should be_an_instance_of Book
     end
   end
 
