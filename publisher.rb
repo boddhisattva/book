4f2dafa13f32c04c31a52a7da650ea76e18763c9
boddhisattva <mail4mohnish@yahoo.in>
2013-09-08 18:43:04 +0530

    sample test for a constant

--- publisher.rb
+++ publisher.rb
@@ -1,5 +1,6 @@
 module Publisher
   # To change this template use File | Settings | File Templates.
+  MAX = 5
   def Publisher.publisher_name
     puts "new_publisher coming from publisher module"
   end
