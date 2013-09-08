26504c6a8cd0ef60204c610a53e60a83fa044887
boddhisattva <mail4mohnish@yahoo.in>
2013-09-08 16:56:16 +0530

    adding difference between include and extend in ruby

--- book.rb
+++ book.rb
@@ -1,19 +1,30 @@
 class RspecLoopStop < Exception; end
-#require './publisher' requires your module to be in the same directory as current...
+require './publisher' #requires your module to be in the same directory as current...
 # #reference http://stackoverflow.com/questions/9750610/ruby-require-error-cannot-load-such-file
-require_relative 'publisher'
+#require_relative 'publisher' --> doesn't work as of 8/9/13 to comeback and check..
+
+module Month
+  def say_hello_from_month
+    puts "hi from month"
+  end
+end
 
 module Week
   FIRST_DAY = "Sunday"
-  def Week.weeks_in_month
+  def Week.weeks_in_month  #please verify - is this what's referred to as a module method ?
     puts "You have four weeks in a month"
   end
   def Week.weeks_in_year
     puts "You have 52 weeks in a year"
   end
+
+  def say_hello_from_week
+    puts "hi from week"
+  end
 end
 
 class Book
+  extend Month
   include Week
   include Publisher# even without the include stmnt Publisher.get_publisher works..
   attr_accessor :books
@@ -75,4 +86,11 @@ Book.new(books).get_publisher #will work
 
 Publisher.publisher_name
 Week.weeks_in_year
-#book_details.publisher_name --> please check.. come back 2..
+book_details.say_hello_from_week # include module_name - allows you access to module methods
+puts "calling from an the class:- "
+Book.say_hello_from_month
+book_details.extend(Month) #basically with extend we can use it to extend an entire class or just an object thus the above and below stmnt respectively work.
+puts "calling from an instance method:- "
+book_details.say_hello_from_month # simple way of extending an instance..
+
+#book_details.publisher_name --> doesn't work as we need a "method" to be defined as in the case of:- say_hello_from_week
